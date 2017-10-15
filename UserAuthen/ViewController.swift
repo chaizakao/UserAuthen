8728b87b428eec53f0d434ff8f9042171ef1bb3a
chaizakao <chai_2117@hotmail.com>
2017-10-15 09:48:08 +0700

    UI OK

--- UserAuthen/ViewController.swift
+++ UserAuthen/ViewController.swift
@@ -9,10 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var userDictionaryString = ["key1":"chaiwat", "key2":"54321"]
+    
+    
+    
+    
+    
+    @IBOutlet weak var showLabel: UILabel!
+    
+    
+    @IBOutlet weak var textUser: UITextField!
+    
+   
+    @IBOutlet weak var textPass: UITextField!
+    
+   
+    
+    @IBAction func buttonLogin(_ sender: Any) {
+        
+       /*
+        let usrTextString = userDictionaryString["key1"]
+        let pasTextString = userDictionaryString["key2"]
+        
+        
+         if (textUser.text == (usrTextString) || textPass.text == (pasTextString))
+            {
+        
+                showLabel.text = "ล็อกอินสำเร็จ "
+                showLabel.textColor = UIColor.blue
+            }
+        showLabel.text = "User or Pass Wrong "
+        showLabel.textColor = UIColor.red
+        */
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        showLabel.text = " กรอก USER และ PASS "
+        showLabel.textColor = UIColor.black
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
